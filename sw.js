f0c4ce4dcce15140ec15353afc125f662ab5343e
cosuenda <88199562+cosuenda@users.noreply.github.com>
2026-03-05 11:36:07 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE_NAME='meteo-v1';
+self.addEventListener('install',e=>{
+e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(['./','./index.html','./style.css','./script.js','./manifest.json'])));
+});
+self.addEventListener('fetch',e=>{
+e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
